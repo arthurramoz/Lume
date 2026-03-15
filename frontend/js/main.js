61f3dadc8bb1cce66b8431539ebb2f13a92dbd0c
Bianca Codo <biancanunescodo75@gmail.com>
2026-03-15 14:44:01 -0300

    feat: navbar web admin

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,9 +2,9 @@ import { verifyAccess } from "./hooks/useAuth.js";
 import { initBookList } from "./modules/bookList.js";
 import { initTopbar } from "./modules/topbar.js";
 
-verifyAccess();
+// verifyAccess();
 
 document.addEventListener("DOMContentLoaded", () => {
-  initTopbar();
-  initBookList();
+    initTopbar();
+    initBookList();
 });
