33c01489bf5a2ffa12c9afd11fc96f2a14db9ecd
arthurramoz <arthurmoreiraramos550sp@gmail.com>
2026-03-16 21:35:00 -0300

    fix: js config pathnames

--- frontend/js/modules/navbar.js
+++ frontend/js/modules/navbar.js
@@ -8,19 +8,19 @@ export const initAdminNavbar = () => {
         <img src="/assets/images/logo-lume.svg" alt="Logo Lume" class="logo" />
     </div>
     <div class="middle">
-      <div class="option">
+      <div class="option nav-users">
         <img src="/assets/icons/users-off.svg" alt="Ícone" />
         <a href="/admin/users.html">Usuários</a>
       </div>
-      <div class="option">
+      <div class="option nav-dashboard">
         <img src="/assets/icons/dashboard-off.svg" alt="Ícone" />
         <a href="/admin/dashboard.html">Dashboard</a>
       </div>
-      <div class="option">
+      <div class="option nav-orders">
         <img src="/assets/icons/orders-off.svg" alt="Ícone" />
         <a href="/admin/orders.html">Pedidos</a>
       </div>
-      <div class="option">
+      <div class="option nav-cupons">
         <img src="/assets/icons/cupons-off.svg" alt="Ícone" />
         <a href="/admin/cupons.html">Cupons</a>
       </div>
@@ -32,6 +32,43 @@ export const initAdminNavbar = () => {
       </div>
     </div>
   `;
+
+  const path = window.location.pathname;
+
+  if (path.includes("/admin/users" || path.includes("/pages/admin/users"))) {
+    const usersOption = document.querySelector(".nav-users");
+    const usersIcon = usersOption?.querySelector("img");
+
+    if (usersOption) usersOption.style.color = "var(--color-primaria100)";
+    if (usersIcon) usersIcon.src = "/assets/icons/users-on.svg";
+  }
+
+  if (
+    path.includes("/admin/dashboard" || path.includes("/pages/admin/dashboard"))
+  ) {
+    const dashboardOption = document.querySelector(".nav-dashboard");
+    const dashboardIcon = dashboardOption?.querySelector("img");
+
+    if (dashboardOption)
+      dashboardOption.style.color = "var(--color-primaria100)";
+    if (dashboardIcon) dashboardIcon.src = "/assets/icons/dashboard-on.svg";
+  }
+
+  if (path.includes("/admin/orders" || path.includes("/pages/admin/orders"))) {
+    const ordersOption = document.querySelector(".nav-orders");
+    const ordersIcon = ordersOption?.querySelector("img");
+
+    if (ordersOption) ordersOption.style.color = "var(--color-primaria100)";
+    if (ordersIcon) ordersIcon.src = "/assets/icons/orders-on.svg";
+  }
+
+  if (path.includes("/admin/cupons" || path.includes("/pages/admin/cupons"))) {
+    const cupomOption = document.querySelector(".nav-cupons");
+    const cupomIcon = cupomOption?.querySelector("img");
+
+    if (cupomOption) cupomOption.style.color = "var(--color-primaria100)";
+    if (cupomIcon) cupomIcon.src = "/assets/icons/cupons-on.svg";
+  }
 
   const logo = document.querySelector(".logo");
   const logoutBtn = document.querySelector("#logout-btn");
